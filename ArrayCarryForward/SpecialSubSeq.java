b58024a367fd63b5f99728be0aae440855348593
Rohit Chaudhary <rohitchaudhary200400@gmail.com>
2022-10-25 19:03:48 +0530

    commit

---
+++ ArrayCarryForward/SpecialSubSeq.java
@@ -0,0 +1,21 @@
+package ArrayCarryForward;
+
+/**
+ * SpecialSubSeq
+ */
+public class SpecialSubSeq {
+
+    public static void main(String[] args) {
+        System.out.println(solve("GUGPUAGAFQBMPYAGGAAOALAELGGGAOGLGEGZ"));
+    }
+    public static int solve(String A) {
+        int cntG = 0;
+        int cntnumberOfSubSeq = 0;
+        for( int index = A.length()-1; index>=0; index--){
+            //if(A.charAt(index) == 'A' && cntG==0 ) continue;
+            if(A.charAt(index) =='G') cntG++;
+            else if(A.charAt(index) == 'A'&& cntG!=0) cntnumberOfSubSeq = cntnumberOfSubSeq%1000000007;
+        }
+        return cntnumberOfSubSeq%1000000007 ;
+    }
+}
