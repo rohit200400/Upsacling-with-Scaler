6b157a3578e8fcfe6fca9a091e7526f3506bfca2
Rohit <rohitchaudhary200400@gmail.com>
2023-11-19 11:02:55 +0530

    Solved fer more problems

--- ProblemSolvingQuestions/src/MergeIntervals.java
+++ ProblemSolvingQuestions/src/MergeIntervals.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class MergeIntervals {
     public static void main(String[] args) {
---
+++ ProblemSolvingQuestions/src/Practice.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Practice {
+    public static void main(String[] args) {
+//        System.out.println(String.valueOf(8));
+//        System.out.println(String.valueOf((char)('d'+1)));
+        String x = "";
+        System.out.println(x);
+        x+= '9';
+        System.out.println(x);
+        Integer n = Integer.valueOf(x);
+        System.out.println(n);
+        int[] arr = new int[10];
+        Arrays.fill(arr, -1);
+        Set<Integer> set = new HashSet<>();
+        set.add(2);
+        set.add(2);
+        System.out.println(set.contains(2));
+        for (Integer k: set
+             ) {
+            System.out.println(k);
+        }
+        HashMap<Integer, String> map = new HashMap<>();
+
+    }
+}
